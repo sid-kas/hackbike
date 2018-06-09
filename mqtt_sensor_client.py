697b727773b5c9695b975d0261b609ced9462e72
sidneni <siddhartha.kasaraneni@gmail.com>
2018-06-09 02:47:04 +0200

    --

--- mqtt_sensor_client.py
+++ mqtt_sensor_client.py
@@ -1,55 +1,44 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import sys
 sys.path.insert(0,"..//")
+
 import PCF8591 as ADC
 import time
 import paho.mqtt.client as mqtt
 import json
+import smbus
+import math
+
+from sensor_utils import *
 
 THINGSBOARD_HOST = '0.0.0.0'
-ACCESS_TOKEN = 'joystick'
-
-
-def setup():
-	ADC.setup(0x48)					# Setup PCF8591
-	global state
-
-def direction():	#get joystick result
-	state = ['home', 'up', 'down', 'left', 'right', 'pressed']
-	i = 0
-
-	if ADC.read(0) <= 5:
-		i = 1		#up
-	if ADC.read(0) >= 250:
-		i = 2		#down
-
-	if ADC.read(1) >= 250:
-		i = 3		#left
-	if ADC.read(1) <= 5:
-		i = 4		#right
-
-	if ADC.read(2) == 0:
-		i = 5		# Button pressed
-
-	if ADC.read(0) - 125 < 15 and ADC.read(0) - 125 > -15	and ADC.read(1) - 125 < 15 and ADC.read(1) - 125 > -15 and ADC.read(2) == 255:
-		i = 0
-	
-	return state[i]
+ACCESS_TOKEN = 'hacknbike'
 
 def on_connect(client, userdata, rc, *extra_params):
     print('Connected with result code ' + str(rc))
-    client.subscribe('pi/joystick')
-    client.publish('pi/joystick', "Connected: "+ direction(), 1)
+	accel_xout = read_word_2c(0x3b)
+    accel_yout = read_word_2c(0x3d)
+    accel_zout = read_word_2c(0x3f)
+	terrain = {'x':get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled), 'y': get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)}
 
+    client.subscribe('pi/#')
+    client.publish('pi/joystick', "joystick: "+ direction())
+	client.publish('pi/terrain', 'terrain: '+ str(terrain) )
 
 def on_message(client, userdata, msg):
     data = direction()
+	accel_xout = read_word_2c(0x3b)
+    accel_yout = read_word_2c(0x3d)
+    accel_zout = read_word_2c(0x3f)
+	terrain = {'x':get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled), 'y': get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)}
+
     if data == 'home':
-        client.publish('pi/joystick', 'spam', 1)
+        client.publish('pi/joystick', 'spam')
     elif data == 'pressed':
-        client.publish('pi/joystick', 'spam', 1)
+        client.publish('pi/joystick', 'spam')
     else:
-        client.publish('pi/joystick', data, 1)
+        client.publish('pi/joystick', data)
+		client.publish('pi/terrain',str(terrain))
         
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -62,5 +51,5 @@ if __name__ == '__main__':
 	setup()
 	try:
 		client.loop_forever()
-	except KeyboardInte
-        	client.loop_stop(force=True)
+	except KeyboardInterrupt:
+		client.loop_stop(force=True)
---
+++ sensor_utils.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import sys
+sys.path.insert(0,"..//")
+
+import PCF8591 as ADC
+import time
+import paho.mqtt.client as mqtt
+import json
+import smbus
+import math
+
+bus = smbus.SMBus(1) 
+def read_word(adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
+
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+	
+def setup():
+	ADC.setup(0x48)				
+	global state
+	address = 0x68 
+	bus.write_byte_data(address, power_mgmt_1, 0)
+
+def direction():	#get joystick result
+	state = ['home', 'up', 'down', 'left', 'right', 'pressed']
+	i = 0
+
+	if ADC.read(0) <= 5:
+		i = 1
+	if ADC.read(0) >= 250:
+		i = 2
+	if ADC.read(1) >= 250:
+		i = 3
+	if ADC.read(1) <= 5:
+		i = 4
+	if ADC.read(2) == 0:
+		i = 5
+	if ADC.read(0) - 125 < 15 and ADC.read(0) - 125 > -15	and ADC.read(1) - 125 < 15 and ADC.read(1) - 125 > -15 and ADC.read(2) == 255:
+		i = 0
+
+	return state[i]
